2e12ecfe73fa14a9455d2fb84a9aa9759391c583
vishal <vishalrana9915@gmail.com>
2023-02-16 10:32:46 +0530

    Finding height of binary tree

---
+++ heightOfBinaryTree.go
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+    if root == nil{
+        //has root value nil
+        return 0
+    }
+
+    
+    return int(math.Max(float64(maxDepth(root.Left)), float64(maxDepth(root.Right)))) +1
+}
